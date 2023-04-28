ce5d8a12290f821c3128c941e2286b12ae676404
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-04-27 18:41:57 -0700

    adding variables

--- Develop/script.js
+++ Develop/script.js
@@ -18,7 +18,23 @@ $(function () {
         localStorage.setItem(timeBlock, userInput);
         console.log('time-block saved from user input' + timeBlock)
     }
-  })
+
+    $('time-block').each(function() {
+      var timeBlock = $(this).attr('id')
+      var savedUserInput = localStorage.getItem(timeBlock)
+      var currentTime = parseInt(timeBlock.split('-')[1]);
+      
+    }
+
+
+
+
+
+
+
+
+
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
